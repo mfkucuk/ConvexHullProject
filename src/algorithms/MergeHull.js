2bbb2be95c9c118200f7266041431149ddc9ba94
Mehmet Feyyaz Kucuk <mfkucuk555@gmail.com>
2024-05-06 03:21:01 +0300

    help: im stuck

--- src/algorithms/MergeHull.js
+++ src/algorithms/MergeHull.js
@@ -1,27 +1,31 @@
 import { ConvexHull } from "../ConvexHull.js";
-import { mergeConvexHulls } from "../lib/mergeConvexHulls.js";
 import { mergeSort } from "../lib/mergeSort.js"
+import { mod } from "../lib/mod.js";
+import { orientation } from "../lib/orientation.js";
 import { GrahamScan } from "./GrahamScan.js";
 
 export class MergeHull {
     /**
      * 
-     * Generates a convex hull for a set of given points using Quick Hull algorithm.
+     * Generates a convex hull for a set of given points using Merge Hull algorithm.
      * 
      * @param {Object[]} S - List of points to generate the convex hull. 
      */
     static construct(S) {
         
-        if (S.length <= 3) {
+        if (S.length <= 6) {
             return GrahamScan.construct(S);
         }
-        
-        const sortedPoints = mergeSort(S, S[0]);
+
+        S.sort((a, b) => a.x - b.x);
+
+        const leftSubset = MergeHull.construct(S.slice(0, Math.floor(S.length / 2)));
+        const rightSubset = MergeHull.construct(S.slice(Math.floor(S.length / 2)));
     
-        const leftSubset = MergeHull.construct(sortedPoints.slice(0, Math.floor(sortedPoints.length / 2)));
-        const rightSubset = MergeHull.construct(sortedPoints.slice(Math.floor(sortedPoints.length / 2) + 1));
+        return this.#mergeConvexHulls(leftSubset, rightSubset);
+    }
+
+    static #mergeConvexHulls(hull1, hull2) {
     
-        return mergeConvexHulls(leftSubset, rightSubset);
     }
 }
-
---
+++ src/lib/mod.js
@@ -0,0 +1,7 @@
+export function mod(number, divisor) {
+    if (number >= 0) {
+        return number % divisor;
+    }
+
+    return number + divisor;
+}
